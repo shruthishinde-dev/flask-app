cf101ea20fba798fa14405a6559eda96d6a0318b
Shruthi <shruthishinde33@gmail.com>
2024-07-14 20:56:17 -0500

    add procfile

--- src/app.py
+++ src/app.py
@@ -1,17 +1,15 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+@app.route('/echo', methods=['POST'])
+def echo():
+    user_input = request.form['user_input']
+    return f"You entered: {user_input}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
